bb56451f8a0b0cf13c7361e06ff7131a64bc2955
Edward Pizzurro Fortun <edwardpizzurro@MacBook-Pro-de-Edward.local>
2014-09-13 11:00:15 -0430

    other class

---
+++ anotherTest.h
@@ -0,0 +1,13 @@
+//
+//  anotherTest.h
+//  Git
+//
+//  Created by Edward Pizzurro Fortun on 13/09/14.
+//  Copyright (c) 2014 Edwjon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface anotherTest : NSObject
+
+@end
---
+++ anotherTest.m
@@ -0,0 +1,13 @@
+//
+//  anotherTest.m
+//  Git
+//
+//  Created by Edward Pizzurro Fortun on 13/09/14.
+//  Copyright (c) 2014 Edwjon. All rights reserved.
+//
+
+#import "anotherTest.h"
+
+@implementation anotherTest
+
+@end
